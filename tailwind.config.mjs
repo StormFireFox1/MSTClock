17a6b9f2dfff188d32d5966062631fe251c4b0a0
Matei-Alexandru Gardus <matei@gard.us>
2024-03-26 20:20:31 -0700

    Initial commit
    Done after a one-hour speedrun.

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'rowdies': ['Rowdies', 'sans-serif'],
+			'righteous': ['Righteous', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'cadet': '#171738',
+				'dim-gray': '#726E75',
+				'snow': '#FFFCFF'
+			}
+		},
+	},
+	plugins: [],
+}
